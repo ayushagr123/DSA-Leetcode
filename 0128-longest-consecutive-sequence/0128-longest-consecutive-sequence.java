25d03cf6b1a2cb24748f139fa7f49cd1f92ecc67
Ayush Agrawal <ayushagr.work@gmail.com>
2026-02-08 19:19:57 +0530

    Time: 32 ms (41.62%), Space: 95.9 MB (37.73%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,23 +1,25 @@
-// Bettter approach => 
-// 1) First sort the array
-// 2) Then traverse through the entire array and look for longest consceutive sequence and return its length.
 class Solution {
     public int longestConsecutive(int[] nums) {
-        Arrays.sort(nums);
-        int length=1;
-        int maxLength=1;
-        if(nums.length>0){
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i+1]-nums[i]==1){
-                length++;
-                maxLength = Math.max(length,maxLength);
+        Set<Integer> elements = new HashSet<>();
+        int n=nums.length;
+        int length = 1;
+        int maxLength = 1;
+        if(n>0){
+            for(int i=0;i<n;i++){
+                elements.add(nums[i]);
             }
-            else if(nums[i+1]-nums[i]==0){
-                continue;
+            for(int i:elements){
+                if(elements.contains(i-1)) continue;
+                else{
+                    length =1;
+                    while(elements.contains(i+1)){
+                        length++;
+                        i++;
+                    }
+                    maxLength=Math.max(maxLength,length);
+                }
             }
-            else length=1;
-        }
-        return maxLength;
+            return maxLength;
         }
         else return 0;
 
