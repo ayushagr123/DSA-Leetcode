ea905e99997d6b6b1c328f68cf06fd64c2d34559
Ayush Agrawal <ayushagr.work@gmail.com>
2026-02-22 22:53:18 +0530

    Time: 1 ms (94.84%), Space: 43.4 MB (86.57%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -8,17 +8,20 @@ class Solution {
         // calculate nCr
         for(int row=1;row<=numRows;row++){
             List<Integer> pascalInner = new ArrayList<>();
+            int ans =1 ;
             for(int col=1;col<=row;col++){
-                int ans = 1;
-                for(int j=0;j<col-1;j++){
-                    ans = ans*(row-1-j);
-                    ans = ans/(j+1);
+                if(col>1){
+                    ans*=(row-col+1);
+                    ans/=(col-1);
                 }
+                // for(int j=0;j<col-1;j++){
+                //     ans = ans*(row-1-j);
+                //     ans = ans/(j+1);
+                // }
                 pascalInner.add(ans);
             }
             pascal.add(pascalInner);
         }
-
         return pascal;
     }
 }
