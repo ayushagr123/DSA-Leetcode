d6140e3d0b0369efd619f321ec66dd27de9eda4d
Ayush Agrawal <ayushagr.work@gmail.com>
2026-04-24 02:18:43 +0530

    Time: 0 ms (100%), Space: 43.9 MB (29.36%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,25 +1,28 @@
 class Solution {
     public int search(int[] nums, int target) {
-        //Simple binary search can't be applied because array is not sorted 
-        //If we try to sort the given array that would take O(nlogn)
-        //Hence what we can do is => we can divide the array in two parts since both of the
-        //parts are sorted we can apply binary search on it.
-        int n = nums.length;
         int start = 0;
-        int end = n-1;
+        int end = nums.length-1;
         while(start<=end){
             int mid = (start+end)/2;
             if(nums[mid]==target) return mid;
-            else if(nums[mid]>=nums[start]){  //We are in left sorted half
-                if(nums[start]<=target && target<nums[mid]) end = mid-1; //Target is in range(start..mid)
-                else  start = mid+1;
-            } 
-            else if(nums[mid]<nums[start]){ //We are in right sorted half
-                if(nums[mid]<target && target<=nums[end]) start = mid+1;
-                else end = mid-1;    
-            }    
+            else if(nums[mid]>=nums[start]){  //Left sorted half
+                if(target<nums[mid] && target>=nums[start]){
+                    end = mid-1;
+                }
+                else{
+                    start = mid+1;
+                }
+            }
+
+            else {  //Right sorted half
+                if(target>nums[mid] && target<=nums[end]){
+                    start = mid+1;
+                }
+                else{
+                    end = mid-1;
+                }
+            }
         }
         return -1;
     }
 }
-
