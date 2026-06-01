9387c3bba9f240e891cf43ce071b02efc560031a
Ayush Agrawal <ayushagr.work@gmail.com>
2026-06-01 23:51:12 +0530

    Time: 0 ms (100%), Space: 42.9 MB (59.59%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,87 +1,34 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        // List<Integer> spiral = new ArrayList<>();
-        // int m = matrix.length;
-        // int n= matrix[0].length;
-        // int rowStart =0, colStart = 0;
-        // int colEnd = n;
-        // int rowEnd = m;
-        // boolean rightMov=false;
-        // boolean bottomMov =false;
-        // //Right
-        // while(rowStart<rowEnd && colStart<colEnd){
-        //     //Right
-        //     for(int j=colStart;j<colEnd;j++){
-        //         spiral.add(matrix[rowStart][j]);
-        //         rightMov = true;
-        //     }
-        //     //Bottom
-        //     for(int i=rowStart+1;i<rowEnd;i++){
-        //         spiral.add(matrix[i][colEnd-1]);
-        //         bottomMov = true;
-        //     }
-        //     //Left
-        //     if(bottomMov){
-        //         for(int j=colEnd-1-1;j>=colStart;j--){
-        //             spiral.add(matrix[rowEnd-1][j]);
-        //             rightMov = true; 
-        //         }
-        //     }
-        //     //Top
-        //     if(rightMov){
-        //         for(int i=rowEnd-1-1;i>=rowStart+1;i--){
-        //         spiral.add(matrix[i][colStart]);
-        //     }
-        // }
-        // rowEnd--;
-        // colEnd--;
-        // rowStart++;
-        // colStart++;
-        // rightMov = false;
-        // bottomMov = false;
-        // }
-        // return spiral;
-
-
-
-        List<Integer> spiral = new ArrayList<>();
         int m = matrix.length;
         int n = matrix[0].length;
-        int rowStart = 0, colStart = 0;
-        int colEnd = n;
-        int rowEnd = m;
-
-        while (rowStart < rowEnd && colStart < colEnd) {
-            // Right
-            for (int j = colStart; j < colEnd; j++) {
-                spiral.add(matrix[rowStart][j]);
+        int rowStart = 0;
+        int colStart = 0;
+        int rowEnd = m-1;
+        int colEnd = n-1;
+        List<Integer> spiral = new ArrayList<>();
+        while(colStart<=colEnd && rowStart<=rowEnd){
+            for(int i = colStart;i<=colEnd;i++){
+                spiral.add(matrix[rowStart][i]);
             }
-            
-            // Bottom
-            for (int i = rowStart + 1; i < rowEnd; i++) {
-                spiral.add(matrix[i][colEnd - 1]);
+            rowStart++;
+            for(int i = rowStart;i<=rowEnd;i++){
+                spiral.add(matrix[i][colEnd]);
             }
-            
-            // Left
-            // FIX: Only go left if we are not on the same row where we started
-            if (rowStart < rowEnd - 1) { 
-                for (int j = colEnd - 2; j >= colStart; j--) {
-                    spiral.add(matrix[rowEnd - 1][j]);
+            colEnd--;
+            if(rowStart<=rowEnd){
+                for(int i = colEnd;i>=colStart;i--){
+                    spiral.add(matrix[rowEnd][i]);
                 }
-            }
-            
-            // Top
-            // FIX: Only go up if we are not on the same column where we went down
-            if (colStart < colEnd - 1) { 
-                for (int i = rowEnd - 2; i > rowStart; i--) {
-                    spiral.add(matrix[i][colStart]);
-                }
-            }
             
             rowEnd--;
-            colEnd--;
-            rowStart++;
-            colStart++;
+            }
+            if(colStart<=colEnd){
+                for(int i = rowEnd;i>=rowStart;i--){
+                    spiral.add(matrix[i][colStart]);
+                }
+                colStart++;
+            }
         }
         return spiral;
     }
