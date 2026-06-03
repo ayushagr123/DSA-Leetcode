d96265fbfa6522f745248b777f27391595f8cbe2
Ayush Agrawal <ayushagr.work@gmail.com>
2026-06-03 20:05:53 +0530

    Time: 3 ms (73.82%), Space: 52.4 MB (73.55%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,36 +1,35 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         List<Integer> list = new ArrayList<>();
-        int candidate1=0;
-        int candidate2=0;
-        int count1=0;
-        int count2=0;
-            for(int num:nums){
-                if(num == candidate1) count1++;
-                else if(num == candidate2) count2++;
-                else if(count1 == 0){
-                    candidate1 = num;
-                    count1 = 1;
-                }
-                else if(count2 == 0){
-                    candidate2 = num;
-                    count2 = 1;
-                }
-                else{
-                    count1--;
-                    count2--;
-                }
+        int elem1 = 0;
+        int elem2 = 0;
+        int count1 = 0;
+        int count2 = 0;
+        //Apply Moore's voting algorithm for 2 elements
+        for(int i = 0;i<nums.length;i++){
+            if(count1 == 0 && elem2 != nums[i]){
+                elem1 = nums[i];
+                count1++;
             }
-            int freq1=0;
-            int freq2=0;
-            for(int num:nums){
-                if(num == candidate1) freq1++;
-                else if(num == candidate2) freq2++;
+            else if(count2 == 0 && elem1 != nums[i]){
+                elem2 = nums[i];
+                count2++;
             }
-
-            if(freq1>nums.length/3) list.add(candidate1);
-            if(freq2>nums.length/3) list.add(candidate2);
-            
+            else if(nums[i] == elem1) count1++;
+            else if(nums[i] == elem2) count2++;
+            else {
+                count1--;
+                count2--;
+            }
+        }
+        //Check actual count of the majority element 
+        int freq1 = 0;int freq2 = 0;   
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i] == elem1) freq1++;
+            else if(nums[i] == elem2) freq2++;
+        }
+        if(freq1>nums.length/3) list.add(elem1);
+        if(freq2>nums.length/3) list.add(elem2);
         return list;
     }
 }
