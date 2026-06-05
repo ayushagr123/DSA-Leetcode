a0d9a0d2b530fd63bf152d0ef11ff4b265a70e85
Ayush Agrawal <ayushagr.work@gmail.com>
2026-06-05 23:16:31 +0530

    Time: 20 ms (64.6%), Space: 45.8 MB (45.44%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,26 +1,30 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        // O(N^3) => TC
-        int n= nums.length;
-        Set<List<Integer>> outerSet = new HashSet<>();
+        //Same 3 sum approach first fix the first element, then second element and use 2 pointer to find the sum
         Arrays.sort(nums);
-        for(int i =0;i<n-3;i++){
-            for(int m=i+1;m<n-2;m++){
-                int j =m+1;
-                int k = n-1;
-                while(j<k){
-                    long sum = (long) nums[i]+nums[m]+nums[j]+nums[k];
-                    if(sum>target) k--;
-                    else if(sum<target) j++;
-                    else {
-                        List<Integer> innerList = Arrays.asList(nums[i],nums[m],nums[j],nums[k]);
-                        outerSet.add(innerList);
-                        j++;
-                        k--;
+        List<List<Integer>> outer = new ArrayList<>();
+        //Fix the first element
+        for(int l = 0;l<nums.length;l++){
+            if(l>0 && nums[l] == nums[l-1]) continue;
+            //Fix second element
+            for(int k = l+1;k<nums.length;k++){
+                if(k>l+1 && nums[k] == nums[k-1]) continue;
+                int i = k+1;
+                int j = nums.length-1;
+                while(i<j){
+                    long sum =(long) nums[i]+nums[j]+nums[k]+nums[l];
+                    if(sum==target){
+                        outer.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
+                        i++;
+                        j--;
+                        while(i<j && nums[i]==nums[i-1]) i++;
+                        while(i<j && nums[j] == nums[j+1]) j--;
                     }
-                } 
+                    else if(sum<target) i++;
+                    else j--;
+                }
             }
         }
-        return new ArrayList<>(outerSet);
+        return outer;
     }
 }
