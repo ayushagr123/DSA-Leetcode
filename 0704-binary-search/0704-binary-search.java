7816cde57d58e184b838acc3bdfae620df9880c2
Ayush Agrawal <ayushagr.work@gmail.com>
2026-06-09 22:11:54 +0530

    Time: 0 ms (100%), Space: 48.3 MB (49.06%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,19 +1,12 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n = nums.length;
-        return divide(nums,0,n-1,target);
+        return divide(nums,target,0,nums.length-1);
     }
-    public int divide(int[] nums, int start,int end,int target){
-        int mid = start + (end-start)/2;
+    public int divide(int[] nums, int target, int start ,int end){
         if(start>end) return -1;
-        else if(target>nums[mid]){
-            start = mid+1;
-            return divide(nums,start,end,target);
-        }
-        else if(target<nums[mid]){
-            end = mid-1;
-            return divide(nums,start,end,target);
-        }
-        else  return mid;
+        int mid = (start+end)/2;
+        if(nums[mid]==target) return mid;
+        else if(target>nums[mid]) return divide(nums,target,mid+1,end);
+        else return divide(nums,target,start,mid-1);
     }
 }
