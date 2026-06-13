f1e9ce0a42bc296f794251e76b84e968344ea945
Ayush Agrawal <ayushagr.work@gmail.com>
2026-06-13 16:59:45 +0530

    Time: 0 ms (100%), Space: 52.9 MB (37.27%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,21 +1,21 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        if(nums.length==1) return nums[0];
-        int low = 0;
-        int high = nums.length-1;
-        while((high-low)>2){
+        int n = nums.length;
+        int low = 1;
+        int high = n-2;
+        if(n==1) return nums[0];
+        if(nums[0]!=nums[1]) return nums[0];
+        if(nums[n-1] != nums[n-2]) return nums[n-1];
+        while(low<=high){
             int mid = (low+high)/2;
-            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) return nums[mid];
-            if(mid%2 == 0){
-                if(nums[mid]!=nums[mid+1]) high = mid;
-                else low = mid;
-            } 
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) return nums[mid];
+            else if(mid%2==0 && nums[mid] == nums[mid+1] || mid%2!=0 && nums[mid] == nums[mid-1]){ //We are on the left side and single element is present on the right side
+                low = mid+1;
+            }
             else{
-                if(nums[mid]!=nums[mid+1]) low = mid;
-                else high = mid-1;
+                high = mid-1;
             }
         }
-        if(nums[low]==nums[low+1]) return nums[high];
-        else return nums[low];
+        return -1;
     }
 }
