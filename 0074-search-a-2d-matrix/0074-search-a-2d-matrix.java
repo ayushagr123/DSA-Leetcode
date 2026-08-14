d9d38affe95d6c5209a7bfb0a32baa154f4125ab
Ayush Agrawal <ayushagr.work@gmail.com>
2026-08-15 00:50:09 +0530

    Time: 0 ms (100%), Space: 43.9 MB (44.46%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,30 +1,17 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        // Find the upper bound for the target from last column
+        if(matrix.length == 0) return false;
         int m = matrix.length;
         int n = matrix[0].length;
         int low = 0;
-        int high = m-1;
-        int row = 0;
+        int high = m*n-1;
+        
         while(low<=high){
-            int mid = (low+high)/2;
-            if(matrix[mid][n-1] == target) return true;
-            else if(matrix[mid][n-1]>target){
-                row = mid;
-                high = mid-1;
-            }
+            int mid = (low + high)/2;
+            if(matrix[mid/n][mid%n] == target) return true;
+            else if(matrix[mid/n][mid%n]>target) high = mid-1;
             else low = mid+1;
         }
-
-        //Now apply binary search in the row obtained from previous lower bound
-        low = 0;
-        high = n-1;
-        while(low<=high){
-            int mid = (low+high)/2;
-            if(matrix[row][mid] == target) return true;
-            else if(matrix[row][mid] > target) high = mid-1;
-            else low = mid+1;
-        }
-        return false;
+        return false ;
     }
 }
