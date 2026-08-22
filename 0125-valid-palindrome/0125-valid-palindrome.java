ddcaefce12b144b0a605e6ebd0217dfeec5a187c
Ayush Agrawal <ayushagr.work@gmail.com>
2026-08-23 01:05:38 +0530

    Time: 3 ms (57.56%), Space: 44.2 MB (74.09%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,10 +1,21 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        //First remove all the non-alphanumeric characters from the string
-        String newString = s.replaceAll("[^a-zA-Z0-9]","");
-        String finalstring = newString.toLowerCase();
-        String reverse = new StringBuilder(finalstring).reverse().toString();
-        if(reverse.equals(finalstring)) return true;
-        else return false;
+        //use two pointers and check if first half of string equals other half
+        int i = 0;
+        int j = s.length()-1;
+        while(i<j){
+            while(i<j && !alphaNumeric(s,i)) i++;
+            while(i<j && !alphaNumeric(s,j)) j--;
+            if(Character.toLowerCase(s.charAt(i)) == Character.toLowerCase(s.charAt(j))) {
+                i++;
+                j--;
+            }
+            else return false;
+        }
+        return true;
     }
+    public static boolean alphaNumeric(String s, int i){
+        if(s.charAt(i) >= 'A' && s.charAt(i) <= 'Z' || s.charAt(i) >= 'a' && s.charAt(i) <= 'z' || s.charAt(i) >= '0' && s.charAt(i) <= '9') return true;
+        return false;
+    } 
 }
